481c8d5f04c69a682d57d91f3385bc0a7aed8827
alanahbell10 <63126323+alanahbell10@users.noreply.github.com>
2022-08-04 13:18:33 -0400

    Add files via upload

---
+++ CodeHistory/CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Alanah Bell on 8/4/22.
+//
+
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
---
+++ CodeHistory/CodeHistory/Views/GameView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  CodeHistory
+//
+//  Created by Alanah Bell on 8/4/22.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    @StateObject var viewModel = GameViewModel()
+    
+    var body: some View {
+        ZStack {
+            GameColor.main.ignoresSafeArea()
+            VStack {
+                Text(viewModel.questionProgressText)
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                QuestionView(question: viewModel.currentQuestion)
+            }
+            .foregroundColor(.white)
+            .navigationBarHidden(true)
+            .environmentObject(viewModel)
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
